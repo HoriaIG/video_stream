8c21d3890d43bf17f63d5e1d9a59c696abc36be1
Horia <horia.iotu@gmail.com>
2026-08-21 15:05:56 +0300

    added camera capture+output, needed forced sizing due to lack of auto-neg on the autovideosink

--- main.cpp
+++ main.cpp
@@ -28,7 +28,7 @@ int main(int argc, char *argv[]) {
 
     GList *devices = gst_device_monitor_get_devices(monitor);
 
-    GstDevice *device;
+    GstDevice *device = nullptr;
 
     for (GList *l = devices; l; l = l->next) {
         device = GST_DEVICE(l->data);
@@ -41,13 +41,82 @@ int main(int argc, char *argv[]) {
         GstCaps* capString = gst_device_get_caps(device);
         cout << "Capabilities: " << gst_caps_to_string(capString) << "\n";
 
+        //the next part can be removed if you just want the last camera interface
+        //this can lead to some errors if you have multiple video cameras might change later
+        //for me it has to be video2
+        const gchar *path = gst_structure_get_string(propString, "api.v4l2.path");
+        if (path && strcmp(path, "/dev/video2") == 0) {
+            gst_structure_free(propString);
+            break;
+        }
         
         gst_structure_free(propString);
+
     }
+
+    //this is the process pipeline
+    GstElement *pipeline = gst_pipeline_new("camera-pipeline");
+
+    //create the element by using the found camera
+    GstElement *source = gst_device_create_element(device, "camera");
+
+    //this is the endpoint sink display stuff
+    GstElement *sink = gst_element_factory_make("autovideosink", "display");
+
+    //setup some error to doublecheck
+    if (!pipeline || !source || !sink) {
+        cerr << "Failed to create GStreamer elements\n";
+        return 1;
+    }   
 
     gst_device_monitor_stop(monitor);
     gst_object_unref(monitor);
 
+
+    GstElement *capsfilter = gst_element_factory_make("capsfilter", "camera-caps");
+
+    GstCaps *caps = gst_caps_new_simple(
+        "video/x-raw",
+        "format", G_TYPE_STRING, "YUY2",
+        "width", G_TYPE_INT, 640,
+        "height", G_TYPE_INT, 480,
+        "framerate", GST_TYPE_FRACTION, 30, 1,
+        nullptr
+    );
+    g_object_set(capsfilter, "caps", caps, nullptr);
+    gst_caps_unref(caps);
+
+    GstElement *convert = gst_element_factory_make("videoconvert", "convert");
+
+    //need this to bind the pipeline to the source and sink created earlier
+    gst_bin_add_many(GST_BIN(pipeline), source, capsfilter, convert, sink, nullptr);
+
+    //connect the input to output
+    gst_element_link_many(source, capsfilter, convert, sink, nullptr);
+
+    //start the pipeline
+    gst_element_set_state(pipeline, GST_STATE_PLAYING);
+    
+    GstBus *bus = gst_element_get_bus(pipeline);
+
+    while (true) {
+        GstMessage *msg = gst_bus_timed_pop_filtered(
+            bus,
+            GST_CLOCK_TIME_NONE,
+            (GstMessageType)(GST_MESSAGE_ERROR | GST_MESSAGE_EOS)
+        );
+
+        if (msg) {
+            gst_message_unref(msg);
+            break;
+        }
+    }
+
+    //clean up the rest of elements
+    gst_element_set_state(pipeline, GST_STATE_NULL);
+
+    gst_object_unref(bus);
+    gst_object_unref(pipeline);
 
     return 0;
 }
